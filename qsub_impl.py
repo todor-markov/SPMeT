2b31f408aeaab2e3db6b1fd89891f2534b63b65b
Todor Markov <todor.m.markov@gmail.com>
2017-02-22 12:33:05 -0800

    minor changes

--- qsub_impl.py
+++ qsub_impl.py
@@ -6,17 +6,12 @@ import sys
 
 
 def submit_single_qsub_job2(v, script):
-	command = 'qsub -q atlas -l nodes=1:ppn=1 -v %s %s' % (v, script)
+    command = 'qsub -q atlas -l nodes=1:ppn=1 -v %s %s' % (v, script)
 
-	os.system(command)
+    os.system(command)
 
 
-if __name__ == "__main__":   
-    #mscnn_and_regionlets_with_score_intervals
-    # for num_particles in NUM_PARTICLES_TO_TEST:
-    #     submit_single_experiment(use_regionlets=True, use_mscnn=True, num_particles=num_particles, 
-    #                             include_ignored_gt=False, include_dontcare_in_gt=False, 
-    #                             sort_dets_on_intervals=True)
-	for i in range(20):
-        v = 'n=%i,fname=%s' % (50, str(i))
-		submit_single_qsub_job2(v, 'shscrpt.sh')
+if __name__ == "__main__":
+    for i in range(2):
+        var = 'n=%i,fname=%s' % (50, str(i))
+        submit_single_qsub_job2(var, 'shscrpt.sh')
